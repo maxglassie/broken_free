cce0c754467b182442e258ce34e863f6ae9daf08
Max Glassie <msglassie@gmail.com>
2017-04-11 11:39:00 -0600

    User can see countries by region
    Creates a root path that shows all countries by their region
    using a table. Does not display indicators for each country.
    Currently using rake commands to load seed data in the testing environment,
    rather than rerunning each time prior to loading the application. Will
    modify these tests to stub data instead.

---
+++ spec/features/user_can_see_countries_by_region_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "user visits region index page" do
+
+  scenario "user sees countries listed by region" do
+    visit root_path
+
+    expect(current_path).to eq(root_path)
+
+    save_and_open_page
+    expect(page).to have_content("Asia-Pacific")
+    expect(page).to have_content("Afghanistan")
+    expect(page).to have_content(15)
+  end
+end
