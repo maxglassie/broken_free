c70a3f9b40d3fb9434b2e41e099982b678fce53d
Max Glassie <msglassie@gmail.com>
2017-05-25 10:58:40 -0600

    travis CI

--- db/sql/database_queries.sql
+++ db/sql/database_queries.sql
@@ -16,4 +16,26 @@ inner join years y on y.id = i.year_id
 group by r.title, y.year
 order by r.title, y.year DESC;
 
+#avg for region for year
+select r.title, avg(i.economic_freedom_index) as avg_efi, avg(i.corruption_perception_index) as avg_cpi, y.year
+from regions r
+inner join countries c on r.id = c.region_id
+inner join indicators i on i.country_id = c.id
+inner join years y on y.id = i.year_id
+group by r.title, y.year
+having y.year = '2016'
+order by r.title, y.year DESC;
+
+#highest CPI in South America
+select c.name, i.economic_freedom_index, i.corruption_perception_index, y.year
+from regions r
+inner join countries c on r.id = c.region_id
+inner join indicators i on i.country_id = c.id
+inner join years y on y.id = i.year_id
+where r.title = 'East Asia & Pacific'
+  and y.year = '2016'
+  and i.corruption_perception_index IS NOT NULL
+order by i.corruption_perception_index ASC;
+
+
 #EFI ranking for in a year
